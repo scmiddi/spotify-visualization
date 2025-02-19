d813390f8ace1142af6f5dfb4b7d5fbbe2e43ff2
Dominik Schmitt <dominikschmitt712000@gmail.com>
2025-02-19 20:31:17 +0100

    Add spotify types file

--- src/types/spotify.ts
+++ src/types/spotify.ts
@@ -1,1 +1,12 @@
- 
+export interface PlaylistData {
+  playlists: {
+    [key: string]: {
+      name: string;
+      id: string;
+      tracks: {
+        id: string;
+        uri: string;
+      }[];
+    };
+  };
+} 
