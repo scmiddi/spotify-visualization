7543c67cb528f28dcc838b2166fc696547092f25
Dominik Schmitt <dominikschmitt712000@gmail.com>
2025-02-19 20:26:42 +0100

    Fix unused import and 404 error

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
       },
     ],
   },
-  basePath: '/spotify-visualization',
+  basePath: process.env.NODE_ENV === 'production' ? '/spotify-visualization' : '',
 }
 
 module.exports = nextConfig 
--- src/components/PlaylistSelector.tsx
+++ src/components/PlaylistSelector.tsx
@@ -1,7 +1,3 @@
-'use client';
-
-import { PlaylistData } from '@/types/playlist';
-
 interface PlaylistSelectorProps {
   playlists: {
     [key: string]: {
